a5759c12a3b22a3e31f437bc56f296ba40d78624
Mathieu Lecarme <mathieu@garambrogne.net>
2010-07-07 18:10:04 +0200

    purge bucket

--- test/erls3_tests.erl
+++ test/erls3_tests.erl
@@ -7,10 +7,15 @@
 
 start() ->
 	erls3:start(),
+	erls3:clean_bucket(?BUCKET),
 	{ok, ok} = erls3:create_bucket(?BUCKET).
 
-connection_test() ->
-	erls3:head(?BUCKET, "").
+%connection_test() ->
+%	erls3:head(?BUCKET, "").
 
 write_test() ->
-	erls3:write_object(?BUCKET, "test.txt", "hello world", "plain/text", [{"x-amz-beuha", "aussi"}]).
+	%{error, not_found, _} = erls3:write_object("this is not a bucket", "test.txt", "hello world", "plain/text", [{"x-amz-beuha", "aussi"}]),
+	{ok, Hash} = erls3:write_object(?BUCKET, "test.txt", "hello world", "plain/text", [{"x-amz-beuha", "aussi"}]).
+	
+stop() -> erls3:clean_bucket(?BUCKET).
+	
