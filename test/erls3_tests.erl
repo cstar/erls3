087497c8dc447ac8b14b537cb5424eac619b5688
Mathieu Lecarme <mathieu@garambrogne.net>
2010-07-07 15:33:19 +0200

    first test

--- test/erls3_tests.erl
+++ test/erls3_tests.erl
@@ -3,7 +3,14 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -include("erls3.hrl").
--define(BUCKET, 'test.s3.erl');
+-define(BUCKET, "test.s3.erl").
+
+start() ->
+	erls3:start(),
+	{ok, ok} = erls3:create_bucket(?BUCKET).
 
 connection_test() ->
-	erls3:head(BUCKET, '').
+	erls3:head(?BUCKET, "").
+
+write_test() ->
+	erls3:write_object(?BUCKET, "test.txt", "hello world", "plain/text", [{"x-amz-beuha", "aussi"}]).
